e51ea590c796493f89640f7e1b667b6710f2457c
Dominik Horn <dominik.horn@tum.de>
2021-11-06 11:43:49 +0100

    Initial Commit

---
+++ SSHPeeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SSHPeeper
+//
+//  Created by Dominik Horn on 06.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SSHPeeper/SSHPeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSHPeeperApp.swift
+//  SSHPeeper
+//
+//  Created by Dominik Horn on 06.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SSHPeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
